61250b24c9d6bb7b401d89556247db96408b9eb3
Waqas Munir <devwaqas232@gmail.com>
2024-08-25 18:27:35 +0500

    Changed Database Address

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-import authRoutes from "./routes/auth-routes.js";
-import blogRoutes from "./routes/blog-routes.js";
-import userPreferencesRoutes from "./routes/user-preferences-routes.js";
+import authRoutes from './routes/auth-routes.js';
+import blogRoutes from './routes/blog-routes.js';
+import userPreferencesRoutes from './routes/user-preferences-routes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/blogs", blogRoutes);
-app.use("/api/user-preferences", userPreferencesRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/blogs', blogRoutes);
+app.use('/api/user-preferences', userPreferencesRoutes);
 
-const PORT = process.env.PORT || "8000";
+const PORT = process.env.PORT || '8000';
 
 app.listen(PORT, () => {
   mongoose
     .connect(
-      "mongodb+srv://devwaqas232:devwaqas232@blog.leftka8.mongodb.net/blog"
+      'mongodb+srv://devwaqas232:devwaqas232@cluster0.8gerr.mongodb.net/blog'
     )
     .then(() => {
-      console.log("Server is running at Port 6000");
+      console.log('Server is running at Port 6000');
     })
     .catch((error) => {
       console.log(error);
